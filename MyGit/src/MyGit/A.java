cab1f757bbe4dfa74f4b695a069cec70cf7c291c
sun8183 <4sunskyhyun@gmail.com>
2023-12-15 19:49:05 +0900

    devB branch 변경된 작업 커밋하기

--- MyGit/src/MyGit/A.java
+++ MyGit/src/MyGit/A.java
@@ -4,5 +4,6 @@ public class A {
 	
 	A(){
 		System.out.println("A.java");
+		System.out.println("devB branch A.java 수정");
 	}
 }
--- MyGit/src/MyGit/B.java
+++ MyGit/src/MyGit/B.java
@@ -4,5 +4,6 @@ public class B {
 	
 	B(){
 		System.out.println("B.java");
+		System.out.println("devB branch B.java 수정");
 	}
 }
